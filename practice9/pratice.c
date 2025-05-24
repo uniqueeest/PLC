4436ca62c482b2e53cf2a417b9a5d14632e0bb8b
Yoonjae Choi <dbswo9795@gmail.com>
2025-05-24 17:20:24 +0900

    feat: practice9에 test 및 swap 함수 추가

--- practice9/pratice.c
+++ practice9/pratice.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 
+void test(short *ptr)
+{
+  short soft = 0;
+  soft = *ptr;
+  printf("soft =  %d", soft);
+  *ptr = 100;
+}
+
+void swap(short *a, short *b)
+{
+  short temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
 int main()
 {
+  /* 1번 */
   short birthday;
   short *ptr;
   ptr = &birthday;
 
   printf("birthday 변수의 주소는 %p입니다 \n", ptr);
+
+  /* 2번 */
+  short tip = 3;
+  test(&tip);
+  printf("\ntip 변수의 값은 %d입니다 \n", tip);
+
+  /* 3번 */
+  short a = 10, b = 20;
+  printf("a = %d, b = %d\n", a, b);
+  swap(&a, &b);
+  printf("a = %d, b = %d\n", a, b);
+
+  return 0;
 }
