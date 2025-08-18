44f7815d56c905c7b5286263795271c07c602db2
Yoonjae Choi <dbswo9795@gmail.com>
2025-08-18 23:18:08 +0900

    feat: 사용자 핸들러 및 라우터 설정 추가

--- go/playground-1/main.go
+++ go/playground-1/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"main/user"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	// user 인스턴스 초기화
+	repo := user.UserRepository("./db")
+	service := user.UserService(repo)
+	handler := user.UserHandler(service)
+
+	router := gin.Default()
+
+	handler.SetupRoutes(router)
+
+	router.Run(":8080")
 }
