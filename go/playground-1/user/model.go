6134c0526e619f9d8b448a841dcd52ae305a2889
Yoonjae Choi <dbswo9795@gmail.com>
2025-08-19 23:22:53 +0900

    feat: update, delete 모델 추가

--- go/playground-1/user/model.go
+++ go/playground-1/user/model.go
@@ -26,3 +26,30 @@ type CreateUserResponse struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type UpdateUserRequest struct {
+		ID string `json:"id"`
+	Name     *string `json:"name,omitempty"`
+	Gender   *string `json:"gender,omitempty"`
+	Age      *int `json:"age,omitempty"`
+	Email    *string `json:"email,omitempty"`
+	Password *string `json:"password,omitempty"`
+}
+
+type UserResponse struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Gender    string    `json:"gender"`
+	Age       int    `json:"age"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type DeleteUserRequest struct {
+	ID string `json:"id"`
+}
+
+type DeleteUserResponse struct {
+	ID string `json:"id"`
+}
